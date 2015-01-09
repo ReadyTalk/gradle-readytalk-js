7aa835646cff20a12645f4b9fbbcecd079e99f98
Doug Borg <dougborg@dougborg.org>
2015-01-09 11:38:34 -0700

    Correct AsarTask definition.

--- src/main/groovy/com/readytalk/gradle/js/tasks/AsarTask.groovy
+++ src/main/groovy/com/readytalk/gradle/js/tasks/AsarTask.groovy
@@ -1,5 +1,10 @@
 package com.readytalk.gradle.js.tasks
 
+import com.moowork.gradle.node.task.NodeTask
+import org.gradle.api.tasks.InputDirectory
+import org.gradle.api.tasks.OutputFile
+import org.gradle.api.tasks.TaskAction
+
 class AsarTask extends NodeTask {
   @InputDirectory
   def File inputDir
@@ -11,7 +16,7 @@ class AsarTask extends NodeTask {
   void build() {
     outputFile.parentFile.mkdirs();
     script = file('node_modules/asar/bin/asar')
-    args = inputDir.absolutePath, outputFile.absolutePath
+    args = [ inputDir.absolutePath, outputFile.absolutePath ]
     super.exec()
   }
 }
