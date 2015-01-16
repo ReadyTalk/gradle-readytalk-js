9987aacecddeaa8079ce8808c714d9727cbdcb61
Doug Borg <dougborg@dougborg.org>
2015-01-16 09:54:05 -0700

    Fix #9: Update name to AsarPack and add test class.

--- src/main/groovy/com/readytalk/gradle/js/tasks/AsarPack.groovy
+++ src/main/groovy/com/readytalk/gradle/js/tasks/AsarPack.groovy
@@ -3,20 +3,21 @@ package com.readytalk.gradle.js.tasks
 import com.moowork.gradle.node.task.NodeTask
 import org.gradle.api.tasks.InputDirectory
 import org.gradle.api.tasks.OutputFile
-import org.gradle.api.tasks.TaskAction
 
-class AsarTask extends NodeTask {
+class AsarPack extends NodeTask {
   @InputDirectory
   def File inputDir
 
   @OutputFile
   def File outputFile
 
-  @TaskAction
-  void build() {
+  @Override
+  void exec() {
     outputFile.parentFile.mkdirs();
-    script = file('node_modules/asar/bin/asar')
-    args = [ inputDir.absolutePath, outputFile.absolutePath ]
+
+    setScript(project.file('node_modules/asar/bin/asar'))
+    setArgs(['pack', inputDir.absolutePath, outputFile.absolutePath])
+
     super.exec()
   }
 }
---
+++ src/test/groovy/com/readytalk/gradle/js/tasks/AsarPackTest.groovy
@@ -0,0 +1,7 @@
+package com.readytalk.gradle.js.tasks
+
+/**
+ * Created by douglas.borg on 1/9/15.
+ */
+class AsarPackTest extends GroovyTestCase {
+}
